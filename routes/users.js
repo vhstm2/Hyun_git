f33465c330b6f12a27fc1aff14b0461fe5974497
hyun19899 <hyun19899@naver.com>
2018-11-28 10:32:12 +0900

    로긴

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,66 @@
+
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+
+router.post('/login',function (req,res,nect) 
+{
+
+  var username = req.body.username;
+  var password = req.body.password;
+
+  var database = req.app.get("database");
+  var users = database.collection('users');
+
+ // var serverName =  users.find({"username" : username},{"username" : 1});
+ // var serverpass =  users.find({"password" : password},{"password" : 1});
+
+  if(username != undefined && password != undefined)
+  {
+    users.find({"username" : username} , function(err , result)
+    {
+        res.status(200).send("scces");
+    });
+
+
+  }
+});
+
+
+//사용자 등록
+router.post('/add',function(req ,res ,next)
+{
+  var username = req.body.username;
+  var password = req.body.password;
+  var nickname = req.body.nickname;
+  //var score = req.body.score;
+
+  var database = req.app.get("database");
+  var users = database.collection('users');
+  
+  if(username != undefined && password != undefined &&
+     nickname != undefined )
+  {
+    
+    users.insert
+    ([{
+      "username" : username , 
+      "password" : password , 
+      "nickname" : nickname
+      //"score" : score
+    }],function(err , result)
+      {
+          res.status(200).send("success");
+      });
+  }
+
+});
+
+
 
 module.exports = router;
